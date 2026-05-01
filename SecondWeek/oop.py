9411d93ac951ccd9cafaf3d8267035b376003aa2
sushant <sushantbastakoti999@gmail.com>
2026-05-01 13:55:08 +0545

    added second week inside firstweek and done inheritance and abstraction

---
+++ SecondWeek/oop.py
@@ -0,0 +1,19 @@
+class Nepal:
+  def capital(self):
+    print("Capital of nepal is kathmandu")
+
+  def __init__(self):
+    # return 0
+    # init method should return None type not int type  not str or other data type 
+    return None  
+
+
+# c=Nepal()
+# c2=Nepal()
+# c.capital()
+# c2.capital()
+
+
+c=Nepal()
+print(c)
+
